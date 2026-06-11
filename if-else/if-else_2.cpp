f6d2e5f2dc7f411e14eaa75a06f83bd39b06cedd
haushalpandey7-git <haushalpandey7@gmail.com>
2026-06-11 07:53:59 +0545

    Add files via upload

---
+++ if-else/if-else_2.cpp
@@ -0,0 +1,18 @@
+// check pass or fail
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int m;
+    cout<< "Enter total marks:";
+    cin>> m;
+    if(m>=40){
+        cout<< "Pass";
+    }
+    else{
+        cout<< "Fail";
+    }
+
+    return 0;
+}
