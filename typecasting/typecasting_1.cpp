40c6e84746fb63d5cbcbbc4b5b8f53f4099d1c1e
haushalpandey7-git <haushalpandey7@gmail.com>
2026-06-11 17:28:04 +0545

    Add files via upload

---
+++ typecasting/typecasting_1.cpp
@@ -0,0 +1,10 @@
+//converting float to integer
+#include<iostream>
+using namespace std;
+int main(){
+
+    float f=2.03;
+
+    cout<< "Typecasting f = "<< (int)f;
+
+}
---
+++ typecasting/typecasting_2.cpp
@@ -0,0 +1,14 @@
+//converting int to float and calculating average
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=5;
+    int b=6;
+    float avg;
+    cout<<"Typecasting a= "<< (float)a;
+    cout<<"Typecasting b= "<< (float)b;
+    cout<<"Average= "<< (a+b)/2.0;
+
+    return 0;
+}
